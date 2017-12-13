c04d9e664cba4eaeb3431d79e4392a70c35b37b8
Sohil Kaushal <sohilkaushal@gmail.com>
2017-12-13 23:15:23 +0530

    minor changes
    a file is added

--- simple_backup_script.sh
+++ simple_backup_script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#This is a simple backup script that will create a particular folder
 echo Enter the folder to take a backup of
-read source
+src=~/work
 
 echo Enter the destination
-read destination
+destination=~/Desktop
 
-backup_date=$(date +%Y%m%d)
+backup_date=$(date +%Y-%m-%d)
 hostname=$(hostname -s)
-backup_file_name="$hostname-$day.tar.gz"
+backup_file_name="$hostname-$backup_date.tar.gz"
 
 echo Entering $source to backup files to $destination
 echo
 
-tar czf $destination/$backup_file_name $source
+tar czf $destination/$backup_file_name $src
 echo
 
 echo "Backup Finished"
+
